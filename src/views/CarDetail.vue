f1971621711f0fd7f23aca59b1e8aec01d1b28cc
1354478648 <1354478648@qq.com>
2024-02-26 15:24:47 +0800

    2024-02-23:完成汽车信息界面与接口

--- src/views/CarDetail.vue
+++ src/views/CarDetail.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { ref, onMounted, watch, nextTick } from 'vue';
+import { ref } from 'vue';
 import { carDetailListService, carDetailAddService, carDetailDeleteService, carDetailUpdateService } from '@/api/cardetail';
+import { useTokenStore } from '@/stores/token.js'
+const tokenStore = useTokenStore()
 import { ElMessage } from 'element-plus';
-import { rowKey } from 'element-plus/es/components/table-v2/src/common';
 
 // 搜索对象
 const searchData = ref({
     year: '',
     brand: '',
     model: '',
+    category: null,
+    type: null,
     lowPrice: null,
     highPrice: null,
 })
@@ -28,12 +31,12 @@ const cardetail = ref([])
 // 获取管理员分页数据
 const previewSrcList = []
 const getCarDetailList = async () => {
-    console.log(searchData.value.year)
-
     const params = {
         year: searchData.value.year ? searchData.value.year : null,
         brand: searchData.value.brand ? searchData.value.brand : null,
         model: searchData.value.model ? searchData.value.model : null,
+        category: searchData.value.category ? searchData.value.category : null,
+        type: searchData.value.type ? searchData.value.type : null,
         lowPrice: searchData.value.lowPrice ? searchData.value.lowPrice : null,
         highPrice: searchData.value.highPrice ? searchData.value.highPrice : null,
         page: page.value,
@@ -55,6 +58,25 @@ const getCarDetailList = async () => {
 }
 getCarDetailList()
 
+const categoryOptions = [
+    { value: 0, label: "其他" },
+    { value: 1, label: "轿车" },
+    { value: 2, label: "SUV" },
+    { value: 3, label: "MPV" },
+    { value: 4, label: "卡车" },
+    { value: 5, label: "跑车" },
+]
+
+const typeOptions = [
+    { value: 0, label: "其他" },
+    { value: 1, label: "纯电动" },
+    { value: 2, label: "插电混动" },
+    { value: 3, label: "增程" },
+    { value: 4, label: "汽油" },
+    { value: 5, label: "汽油+48V轻混系统" },
+    { value: 6, label: "油电混动" },
+    { value: 7, label: "柴油" },
+]
 
 // 重置搜索对象
 const clearSearchData = () => {
@@ -62,6 +84,8 @@ const clearSearchData = () => {
         year: '',
         brand: '',
         model: '',
+        category: null,
+        type: null,
         lowPrice: null,
         highPrice: null,
     }
@@ -87,6 +111,197 @@ const handleCurrentChange = (newPage) => {
 // 每页条数发生变化时，调用此函数
 const handleSizeChange = (newSize) => {
     size.value = newSize
+    getCarDetailList()
+}
+
+// 汽车信息修改添加基类
+const cardetailId = ref(null)
+const cardetailData = ref({
+    year: '',
+    brand: '',
+    model: '',
+    version: '',
+    image: '',
+    category: null,
+    price: null,
+    type: null,
+    seats: null,
+    describeInfo: ''
+})
+
+// 定义表单校验规则
+const rules = {
+    year: [
+        { required: true, message: '请选择年份', trigger: 'blur' },
+        { pattern: /^\d{4}$/, message: '年份格式不正确', trigger: 'blur' }
+    ],
+    brand: [
+        { required: true, message: '请输入品牌', trigger: 'blur' },
+    ],
+    model: [
+        { required: true, message: '请输入型号', trigger: 'blur' },
+    ],
+    version: [
+        { required: true, message: '请输入版本', trigger: 'blur' },
+    ],
+    image: [
+        { required: true, message: '请上传图片', trigger: 'blur' },
+    ],
+    category: [
+        { required: true, message: '请选择分类', trigger: 'blur' },
+    ],
+    price: [
+        { required: true, message: '请输入指导价', trigger: 'blur' },
+    ],
+    type: [
+        { required: true, message: '请选择驱动类型', trigger: 'blur' },
+    ],
+    seats: [
+        { required: true, message: '请输入座位数', trigger: 'blur' },
+    ],
+    describeInfo: [
+        { required: true, message: '请输入描述信息', trigger: 'blur' },
+    ],
+}
+
+const beforeAvatarUpload = (file) => {
+    const isImg = file.type === 'image/jpeg' || file.type === 'image/png';
+    const isLt2MB = file.size / 1024 / 1024 < 2;
+
+    if (!isImg) {
+        ElMessage.error('上传汽车图片只能是 JPG 或 PNG 格式!');
+        return false // 阻止上传
+    }
+    if (!isLt2MB) {
+        ElMessage.error('上传汽车图片大小不能超过 2MB!');
+        return false // 阻止上传
+    }
+
+    return true
+}
+
+//图片上传成功的回调
+const handleAvatarSuccess = (result) => {
+    //回显图片
+    cardetailData.value.image = result.data.url;
+}
+
+// 清空管理员添加修改对象
+const clearCarDetailData = () => {
+    cardetailData.value = {
+        year: '',
+        brand: '',
+        model: '',
+        version: '',
+        image: '',
+        category: null,
+        price: null,
+        type: null,
+        seats: null,
+        describeInfo: ''
+    }
+}
+
+const handleClose = () => {
+    if (title.value == '修改管理员') {
+        drawer.value = false
+        clearCarDetailData()
+    }
+
+    if (cardetailData.value.year !== '' || cardetailData.value.brand !== '' || cardetailData.value.model !== '' || cardetailData.value.version !== '' || cardetailData.value.image !== '' || cardetailData.value.category !== null || cardetailData.value.price !== null || cardetailData.value.type !== null || cardetailData.value.seats !== null || cardetailData.value.describeInfo !== '') {
+        ElMessageBox.confirm(
+            '需要保存草稿吗？',
+            '提示',
+            {
+                confirmButtonText: '保存',
+                cancelButtonText: '取消',
+                type: 'info',
+            }
+        )
+            .then(() => {
+                drawer.value = false
+                ElMessage({
+                    type: 'success',
+                    message: '保存成功',
+                })
+            }
+            )
+            .catch(() => {
+                drawer.value = false
+                clearCarDetailData()
+            })
+    } else {
+        drawer.value = false
+    }
+}
+
+const addCarDetail = async () => {
+    let result = await carDetailAddService(cardetailData.value)
+    ElMessage.success('添加成功, ID为 ' + result.data.Id)
+    clearCarDetailData()
+    drawer.value = false
+    getCarDetailList()
+}
+
+const delCarDetail = (row) => {
+    ElMessageBox.confirm(
+        `确认删除 ${row.year} ${row.brand}${row.model} ${row.version} 吗？`,
+        '提示',
+        {
+            confirmButtonText: '确认',
+            cancelButtonText: '取消',
+            type: 'warning',
+        }
+    )
+        .then(async () => {
+            await carDetailDeleteService(row.id)
+            ElMessage({
+                type: 'success',
+                message: '删除成功',
+            })
+            getCarDetailList()
+        }
+        )
+        .catch(() => { })
+}
+
+const showDrawer = (row) => {
+    title.value = '修改管理员'
+    drawer.value = true
+    cardetailId.value = row.id
+
+    // 数据回显
+    cardetailData.value.year = row.year
+    cardetailData.value.brand = row.brand
+    cardetailData.value.model = row.model
+    cardetailData.value.version = row.version
+    cardetailData.value.image = row.image
+    cardetailData.value.category = row.category
+    cardetailData.value.price = row.price
+    cardetailData.value.type = row.type
+    cardetailData.value.seats = row.seats
+    cardetailData.value.describeInfo = row.describeInfo
+}
+
+const updateCarDetail = async () => {
+    const params = {
+        id: cardetailId.value,
+        year: cardetailData.value.year,
+        brand: cardetailData.value.brand,
+        model: cardetailData.value.model,
+        version: cardetailData.value.version,
+        image: cardetailData.value.image,
+        category: cardetailData.value.category,
+        price: cardetailData.value.price,
+        type: cardetailData.value.type,
+        seats: cardetailData.value.seats,
+        describeInfo: cardetailData.value.describeInfo,
+    }
+
+    await carDetailUpdateService(params)
+    ElMessage.success("修改成功")
+    clearCarDetailData()
+    drawer.value = false
     getCarDetailList()
 }
 </script>
@@ -120,15 +335,17 @@ const handleSizeChange = (newSize) => {
                 <div class="input-box"></div>
                 <div class="input-box">
                     <el-form-item label="分类:">
-                        <el-select v-model="value" placeholder="请选择分类">
-                            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                        <el-select v-model="searchData.category" placeholder="请选择分类">
+                            <el-option v-for="item in categoryOptions" :key="item.value" :label="item.label"
+                                :value="item.value" />
                         </el-select>
                     </el-form-item>
                 </div>
                 <div class="input-box">
                     <el-form-item label="驱动类型:" style="margin-left: 18px;">
-                        <el-select v-model="value" placeholder="请选择驱动类型">
-                            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                        <el-select v-model="searchData.type" placeholder="请选择驱动类型">
+                            <el-option v-for="item in typeOptions" :key="item.value" :label="item.label"
+                                :value="item.value" />
                         </el-select>
                     </el-form-item>
                 </div>
@@ -141,7 +358,7 @@ const handleSizeChange = (newSize) => {
                 </div>
                 <!-- <span>-</span> -->
                 <div class="input-box">
-                    <el-form-item label="最低价格:" style="margin-left: 18px;">
+                    <el-form-item label="最高价格:" style="margin-left: 18px;">
                         <el-input v-model="searchData.highPrice"
                             :formatter="(value) => `￥ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
                             :parser="(value) => value.replace(/\￥\s?|(,*)/g, '')" placeholder="请输入最高价格" />
@@ -158,17 +375,17 @@ const handleSizeChange = (newSize) => {
             <el-table-column label="ID" sortable prop="id" width="100"></el-table-column>
             <el-table-column label="年份" prop="year" width="100"></el-table-column>
             <el-table-column label="品牌" prop="brand" width="100"></el-table-column>
-            <el-table-column label="型号" prop="model" width="100"> </el-table-column>
-            <el-table-column label="版本" prop="version" width="200"> </el-table-column>
-            <el-table-column label="图片" prop="image" width="150" type="index">
+            <el-table-column label="型号" prop="model" width="100"></el-table-column>
+            <el-table-column label="版本" prop="version" width="200" > </el-table-column>
+            <el-table-column label="图片" prop="image" width="250">
                 <template #default="scope">
-                    <el-image style="width: 125px; height:75px;"
+                    <el-image style="width: 200px; height:150px;"
                         :src="scope.row.image ? scope.row.image : 'src/assets/default-car-image.jpg'" :zoom-rate="1.2"
                         :max-scale="7" :min-scale="0.2" :preview-src-list="previewSrcList" :initial-index="scope.$index"
                         :infinite=false :preview-teleported=true fit="cover" />
                 </template>
             </el-table-column>
-            <el-table-column label="颜色" prop="color" width="75"></el-table-column>
+            <el-table-column label="描述信息" prop="describeInfo" width="300"></el-table-column>
             <el-table-column label="座位数" prop="seats" width="75"></el-table-column>
             <el-table-column label="分类" :filters="[
                 { text: '其他', value: 0 },
@@ -214,15 +431,13 @@ const handleSizeChange = (newSize) => {
                 </template>
             </el-table-column>
             <!-- <el-table-column label="创建时间" sortable prop="createTime" width="200"></el-table-column> -->
-            <el-table-column label="操作">
+            <el-table-column label="操作" width="200">
                 <template #default="{ row }">
                     <el-tooltip content="编辑" placement="top">
-                        <el-button @click="showDrawer(row);" icon="Edit" circle plain type="primary"
-                            :disabled="row.isRoot"></el-button>
+                        <el-button @click="showDrawer(row);" icon="Edit" circle plain type="primary"></el-button>
                     </el-tooltip>
                     <el-tooltip content="删除" placement="top">
-                        <el-button @click="delAdmin(row.id, row.name)" icon="Delete" circle plain type="danger"
-                            :disabled="row.isRoot"></el-button>
+                        <el-button @click="delCarDetail(row)" icon="Delete" circle plain type="danger"></el-button>
                     </el-tooltip>
                 </template>
             </el-table-column>
@@ -238,26 +453,59 @@ const handleSizeChange = (newSize) => {
     </el-card>
 
     <!-- 汽车信息添加&修改 -->
-    <!-- <el-drawer v-model="drawer" :title="title" direction="rtl" :before-close="handleClose">
+    <el-drawer v-model="drawer" :title="title" direction="rtl" :before-close="handleClose">
         <el-form :rules="rules" label-width="80px" :model="cardetailData" class="el-form">
-            <el-form-item prop="name" label="姓名:">
-                <el-input v-model="cardetailData.name" size="large" placeholder="请输入姓名" clearable />
+            <el-form-item prop="year" label="年份:">
+                <el-date-picker v-model="cardetailData.year" type="year" value-format="YYYY" placeholder="请选择年份" />
             </el-form-item>
-            <el-form-item prop="username" label="用户名:">
-                <el-input v-model="cardetailData.username" size="large" placeholder="请输入用户名" clearable />
+            <el-form-item prop="brand" label="品牌:">
+                <el-input v-model="cardetailData.brand" size="large" placeholder="请输入品牌" clearable />
             </el-form-item>
-            <el-form-item prop="phone" label="手机号:">
-                <el-input v-model="cardetailData.phone" size="large" placeholder="请输入手机号" maxlength="11" show-word-limit
-                    clearable />
+            <el-form-item prop="model" label="型号:">
+                <el-input v-model="cardetailData.model" size="large" placeholder="请输入型号" clearable />
+            </el-form-item>
+            <el-form-item prop="version" label="版本:">
+                <el-input v-model="cardetailData.version" size="large" placeholder="请输入版本" clearable />
+            </el-form-item>
+            <el-form-item prop="image" label="图片:">
+                <el-upload class="avatar-uploader" :auto-upload="true" action="/api/file/upload" :show-file-list="false"
+                    :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload" name="file"
+                    :headers="{ 'Authorization': tokenStore.token }">
+                    <el-image v-if="cardetailData.image" :src="cardetailData.image" class="avatar" fit="cover" />
+                    <el-icon v-else class="avatar-uploader-icon">
+                        <Plus />
+                    </el-icon>
+                </el-upload>
+            </el-form-item>
+            <el-form-item prop="category" label="分类:">
+                <el-select v-model="cardetailData.category" placeholder="请选择分类">
+                    <el-option v-for="item in categoryOptions" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+            </el-form-item>
+            <el-form-item prop="type" label="驱动类型:">
+                <el-select v-model="cardetailData.type" placeholder="请选择驱动类型">
+                    <el-option v-for="item in typeOptions" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+            </el-form-item>
+            <el-form-item prop="price" label="指导价:">
+                <el-input v-model="cardetailData.price"
+                    :formatter="(value) => `￥ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+                    :parser="(value) => value.replace(/\￥\s?|(,*)/g, '')" placeholder="请输入指导价" />
+            </el-form-item>
+            <el-form-item prop="seats" label="座位数:">
+                <el-input v-model="cardetailData.seats" type="number" placeholder="请输入座位数" />
+            </el-form-item>
+            <el-form-item prop="describeInfo" label="描述信息:">
+                <el-input v-model="cardetailData.describeInfo" autosize type="textarea" placeholder="请输入描述信息" />
             </el-form-item>
         </el-form>
         <template #footer>
             <div style="flex: auto">
                 <el-button @click="handleClose">取消</el-button>
-                <el-button type="primary" @click="title == '添加汽车信息' ? addAdmin() : updateAdmin();">确认</el-button>
+                <el-button type="primary" @click="title == '添加汽车信息' ? addCarDetail() : updateCarDetail();">确认</el-button>
             </div>
         </template>
-    </el-drawer> -->
+    </el-drawer>
 </template>
 
 <style scoped>
@@ -279,11 +527,10 @@ const handleSizeChange = (newSize) => {
         .search-input {
             display: flex;
             flex-wrap: wrap;
-            width: 75%;
+            width: 80%;
 
             .input-box {
                 width: calc(100% / 4);
-                /* 每列宽度为容器宽度的1/3 */
                 padding: 5px;
                 box-sizing: border-box;
             }
@@ -293,5 +540,21 @@ const handleSizeChange = (newSize) => {
             margin-left: 30px;
         }
     }
+
+    .avatar-uploader {
+        .avatar {
+            width: 200px;
+            height: 150px;
+            display: block;
+        }
+    }
+}
+
+.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 200px;
+    height: 150px;
+    text-align: center;
 }
 </style>
